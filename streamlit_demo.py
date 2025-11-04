beeee80247a40b351ac5bb3d1c38ef59a1d8893e
techinfomalli <55678106+techinfomalli@users.noreply.github.com>
2025-11-05 05:10:28 +0530

    Add files via upload

---
+++ streamlit_demo.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+st.title('Aask anything you want to know')
+
+with st.sidebar:
+    st.title('Ask me the key')
+
+OPENAI_API_KEY = st.text_input('OPEN AI api Key',type="password")
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
+
+
+if not OPENAI_API_KEY:
+    st.info('Enter Open api key to continue')
+    st.stop()
+
+
+llm=ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
+
+st.title("Ask Anything")
+
+question = st.text_input("Enter the question:")
+
+if question:
+    response = llm.invoke(question)
+
+    st.write(response.content)
+
+
